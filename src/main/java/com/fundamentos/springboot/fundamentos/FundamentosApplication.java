d30ed01d7835d14c75e0227ec28fb25f7fa50074
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-22 22:36:00 -0600

    My bean implements two dependcies

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,6 +1,7 @@
 package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependecy;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependecy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,10 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependecy componentDependecy;
 	private MyBean myBean;
+
+	private MyBeanWithDependecy myBeanWithDependecy;
 	@Autowired
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependecy componentDependecy, MyBean myBean) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependecy componentDependecy, MyBean myBean, MyBeanWithDependecy myBeanWithDependecy) {
 		this.componentDependecy = componentDependecy;
 		this.myBean = myBean;
+		this.myBeanWithDependecy = myBeanWithDependecy;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -25,5 +29,6 @@ public class FundamentosApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		componentDependecy.Saludar();
 		myBean.print();
+		myBeanWithDependecy.printWithDependecy();
 	}
 }
