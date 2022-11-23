c7a9928c242742ee216c4abc7eb08d0f0c869a49
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-22 21:47:28 -0600

    Dependecy Inyection

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,11 +1,17 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.component.ComponentDependecy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication {
-
+	private ComponentDependecy componentDependecy;
+	@Autowired
+	public FundamentosApplication(ComponentDependecy componentDependecy) {
+		this.componentDependecy = componentDependecy;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
