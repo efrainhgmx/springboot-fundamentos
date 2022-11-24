0539ede077c5bd03ad45cc4af68492e87c76aba4
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-23 20:51:04 -0600

    spring devtools added

--- src/main/java/com/fundamentos/springboot/fundamentos/controller/TestController.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/controller/TestController.java
@@ -3,14 +3,14 @@ package com.fundamentos.springboot.fundamentos.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
     @RequestMapping
-    @RequestBody
+    @ResponseBody
     public ResponseEntity<String> function() {
-        return new ResponseEntity<String>("Hello from Controller", HttpStatus.OK);
+        return new ResponseEntity<String>("Hello from Controller change cma", HttpStatus.OK);
     }
 }
