2638991da9a1d35140d480d4fd5d0cb0e579767d
Efrain Hernandez <efrainhg2050@gmail.com>
2022-12-05 20:07:16 -0600

    Pojo user class

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/pojo/UserPojo.java
@@ -0,0 +1,37 @@
+package com.fundamentos.springboot.fundamentos.pojo;
+
+public class UserPojo {
+    private String email;
+    private String password;
+    private Integer age;
+
+    public UserPojo(String email, String password, Integer age) {
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
