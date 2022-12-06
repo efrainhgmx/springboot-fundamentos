fb6869d3ea56a339c034f1835d55987c906c18c8
Efrain Hernandez <efrainhg2050@gmail.com>
2022-12-05 20:17:54 -0600

    Dependecy injection

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,7 @@ import com.fundamentos.springboot.fundamentos.bean.MyBeanProperties;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependecy;
 import com.fundamentos.springboot.fundamentos.bean.RestBean;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependecy;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -21,17 +22,21 @@ public class FundamentosApplication implements CommandLineRunner {
     private RestBean restBean;
 
 	private MyBeanProperties myBeanProperties;
+
+	private UserPojo userPojo;
 	@Autowired
 	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependecy componentDependecy,
                                   MyBean myBean,
                                   MyBeanWithDependecy myBeanWithDependecy,
                                   RestBean restBean,
-								  MyBeanProperties myBeanProperties) {
+								  MyBeanProperties myBeanProperties,
+								  UserPojo userPojo) {
 		this.componentDependecy = componentDependecy;
 		this.myBean = myBean;
 		this.myBeanWithDependecy = myBeanWithDependecy;
         this.restBean = restBean;
 		this.myBeanProperties = myBeanProperties;
+		this.userPojo = userPojo;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -44,5 +49,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBeanWithDependecy.printWithDependecy();
 		restBean.uppercase("hola por aca!!!");
 		System.out.println(myBeanProperties.function());
+		System.out.println(userPojo.getEmail());
 	}
 }
