6bdab2f0fd3af18447221599dfaf6beb9a30016f
Efrain Hernandez <efrainhg2050@gmail.com>
2022-12-06 21:11:04 -0600

    Entity annotation

--- src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
@@ -1,4 +1,11 @@
 package com.fundamentos.springboot.fundamentos.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
 }
