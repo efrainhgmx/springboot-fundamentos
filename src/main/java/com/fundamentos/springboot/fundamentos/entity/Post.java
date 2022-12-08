bb8c78bcfbf2b482a56b963b1f1352e92a7aa6da
Efrain Hernandez <efrainhg2050@gmail.com>
2022-12-07 21:06:18 -0600

    constructor and getter and setter in post entity

--- src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
@@ -14,4 +14,37 @@ public class Post {
     private String description;
     @ManyToOne
     private User user;
+
+    public Post() {
+
+    }
+
+    public Post(String description, User user) {
+        this.description = description;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
