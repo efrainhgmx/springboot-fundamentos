4fe749f2126a5a2524e18d50517ed62519348af2
Efrain Hernandez <efrainhg2050@gmail.com>
2022-12-07 21:04:45 -0600

    constructor and getter and setter in user entity

--- src/main/java/com/fundamentos/springboot/fundamentos/entity/User.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/entity/User.java
@@ -23,4 +23,52 @@ public class User {
     @JsonManagedReference
     private List<Post> postList = new ArrayList<>();
 
+    public User() {
+
+    }
+    public User(String name, String email, LocalDateTime birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Post> getPostList() {
+        return postList;
+    }
+
+    public void setPostList(List<Post> postList) {
+        this.postList = postList;
+    }
 }
