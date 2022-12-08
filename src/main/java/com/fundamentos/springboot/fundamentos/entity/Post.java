d05a454202d1c4193a453e4d8da38f204a936f2a
Efrain Hernandez <efrainhg2050@gmail.com>
2022-12-07 20:50:24 -0600

    Adding column for id and post description

--- src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
@@ -1,12 +1,15 @@
 package com.fundamentos.springboot.fundamentos.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "post")
 public class Post {
     @Id
-    public Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_post", nullable = false, unique = true)
+    public Long id;
+
+    @Column(name = "description", length = 255)
+    public String description;
 }
