899bc153eafe0d478f522335c5e3fb809b9a1b20
Efrain Hernandez <efrainhg2050@gmail.com>
2023-01-30 21:27:49 -0600

    Making final in User Rest Controller

--- src/main/java/com/fundamentos/springboot/fundamentos/controller/UserRestController.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/controller/UserRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserRestController(UserRepository userRepository) {
         this.userRepository = userRepository;
